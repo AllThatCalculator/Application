1b58bd2953bb5efac8b31fe9a2cce714171fa362
jiyoung <jiyoung_06@ewhain.net>
2022-05-08 04:41:21 +0900

    [ADD] global style

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./pages/App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import App from "./pages/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const GlobalStyles = createGlobalStyle` 
+    html, body {
+      padding : 0;
+      margin : 0;
+      scroll-behavior: smooth;
+    }
+    *{
+        box-sizing: border-box;
+    }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
-)
+);
