fd5f35dc9a283ad259a95275f11e86b9ed9abfd8
jaeseo <jaeserrr@ewhain.net>
2022-07-21 02:55:55 +0900

    [FIX] profileChange FileReader operate

--- client/src/components/sign-up/Option.js
+++ client/src/components/sign-up/Option.js
@@ -1,14 +1,18 @@
+let now = new Date();
 /**
  * 성별 옵션 - 여, 남
  */
 const OPTIONS_SEX = [
-  { value: "F", name: "여" },
-  { value: "M", name: "남" },
+  { value: "F", name: "여자" },
+  { value: "M", name: "남자" },
 ];
 /**
- * 생년월일 옵션 - 년도 1930~2022
+ * 생년월일 옵션 - 년도 (내림차순)
  */
-const OPTIONS_YEAR = fillOptionsList(1930, 2022);
+const OPTIONS_YEAR = fillOptionsList(
+  now.getFullYear() - 100,
+  now.getFullYear()
+).reverse();
 /**
  * 생년월일 옵션 - 월 1~12
  */
@@ -19,6 +23,7 @@ const OPTIONS_MONTH = fillOptionsList(1, 12);
 const OPTIONS_DATE = fillOptionsList(1, 31);
 /**
  * start ~ end 만큼 연속된 값으로 옵션 채운 array 반환 함수
+ * 년도와 월 둘 다 (순서 상관 없이)에 따라서 일이 생김
  *
  * @param {int, int}
  * start : 처음 숫자
--- client/src/components/sign-up/ProfileChange.js
+++ client/src/components/sign-up/ProfileChange.js
@@ -6,36 +6,70 @@ import { FlexColumnLayout, FlexRowLayout } from "../Layout";
  * 프로필 이미지 컴포넌트
  * 사진 변경 함수 포함
  *
- * @param {string, funtion} info
- * profileImg : 이미지 링크
- * setProfileImg : 이미지 링크 변경하는 함수
+ * @param {string, funtion}
+ * profileImg : 이미지 state
+ * setProfileImg : 이미지 state 변경하는 함수
  */
 function ProfileChange({ profileImg, setProfileImg }) {
   /**
    * Ref를 사용해서 input태그 참조
+   * -> 사진 번경 버튼 누르면, 사진 선택 가능
    */
-  const fileInput = useRef(null);
-  function changeProfileImg() {
-    fileInput.current.click();
+  const imageInput = useRef(null);
+  function changeImageSrc() {
+    imageInput.current.click();
   }
   /**
-   * 선택한 파일로 프로필 사진 업로드 되는 Change 함수
+   * 이미지를 인코딩하여 이미지 state를 변경하는 함수
+   *
+   * <FileReader>
+   * File, Blob 객체를 사용해 특정 파일을 읽어들여 자바스크립트에서 파일에 접근할 수 있도록 도와주는 도구
+   *
+   * <readAsDataURL>
+   * File, Blob 을 읽은 뒤 base64로 인코딩한 문자열을 FileReader 인스턴스의 result라는 속성에 담아줌.
+   * -> 사용자가 선택한 이미지를 base64로 인코딩하여 profileImg 라는 state 안에 넣어줌.
+   *
+   * <FileReader.onload>
+   * FileReader가 성공적으로 파일을 읽어들였을 때 트리거 되는 이벤트 핸들러
+   * -> 이 핸들러 내부에 우리가 원하는 이미지 프리뷰 로직을 넣어줌.
+   *
+   * resolve를 호출하여 Promise를 이행상태로 만들어줌.
+   *
+   * @param {Blob}
+   *
+   * fileBlob : 인코딩할 문자열 image src
+   */
+  function encodeFileToBase64(fileBlob) {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setProfileImg(reader.result);
+        resolve();
+      };
+    });
+  }
+  /**
+   * 변경한 이미지 미리 보기
+   * -> 선택한 이미지로 프로필 사진 변경하는 함수
    * @param {*} event
+   *
+   * event : 선택된 이미지를 참조하기 위한 event
    */
   function onChange(event) {
-    setProfileImg("/img/" + event.target.files[0].name);
+    encodeFileToBase64(event.target.files[0]);
   }
   return (
     <FlexRowLayout>
       <FlexColumnLayout gap="10px">
-        <StyledCircleImg src={profileImg} width="64px" />
-
-        <BtnText text="사진 변경" onClick={changeProfileImg} />
+        <StyledCircleImg src={profileImg} width="64px" height="64px" />
+        <BtnText text="사진 변경" onClick={changeImageSrc} />
         <input
           type="file"
-          ref={fileInput}
-          onChange={onChange}
+          accept="image/*"
           style={{ display: "none" }}
+          ref={imageInput}
+          onChange={onChange}
         />
       </FlexColumnLayout>
     </FlexRowLayout>
