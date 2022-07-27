aae4a78abdf7fa7fce71ae1ee9b6cdaf180cce70
sua bae <bsa0322@ewhain.net>
2022-07-27 11:52:25 +0900

    [ADD] login + create token

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const mariadb = require("../config/database");
-
+const cookieParser = require("cookie-parser");
 const router = express.Router();
 const saltRounds = 10;
 
@@ -26,7 +26,6 @@ router.post("/", (req, res) => {
 
           // 데이터 삽입
           const userInfoData = [
-            req.body.id,
             req.body.email,
             req.body.userName,
             req.body.profileImg,
@@ -35,9 +34,9 @@ router.post("/", (req, res) => {
             req.body.birthdate,
             req.body.job,
           ];
-          const userLoginData = [req.body.id, pw];
+          const userLoginData = [req.body.email, pw];
 
-          const userInfoQuery = `INSERT INTO user_info VALUES(?,?,?,?,?,?,?,?);`;
+          const userInfoQuery = `INSERT INTO user_info VALUES(?,?,?,?,?,?,?);`;
           const userLoginQuery = `INSERT INTO user_login VALUES(?,?);`;
 
           const sql1 = mariadb.format(userInfoQuery, userInfoData);
@@ -67,6 +66,54 @@ router.post("/", (req, res) => {
 /**
  * 로그인 요청
  */
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  // 요청 이메일 DB에서 찾기
+  const sql = `select * from user_login where user_email='${req.body.email}'`;
+
+  mariadb.query(sql, async (err, rows, fields) => {
+    if (!err) {
+      // 계정이 없는 경우
+      if (rows.length === 0) {
+        return res.status(400).send({
+          loginSuccess: false,
+          message: "계정을 찾을 수 없습니다.",
+        });
+      }
+
+      const userLogin = rows[0];
+
+      // 비밀번호 일치하는지 검사
+      const isEqualPw = await bcrypt.compare(`${req.body.pw}`, userLogin.pw);
+      console.log(isEqualPw);
+
+      // 일치하지 않으면
+      if (!isEqualPw) {
+        return res.status(404).send({
+          loginSuccess: false,
+          message: "잘못된 비밀번호입니다.",
+        });
+      }
+      // 일치한다면 토큰 생성
+      const payload = {
+        email: req.body.email,
+      };
+      const token = jwt.sign(payload, process.env.SECRET_KEY, {
+        algorithm: "HS256",
+        expiresIn: "1h",
+      });
+
+      // 토큰을 쿠키에 저장
+      res
+        .cookie("access-token", token, {
+          maxAge: 1000 * 60 * 60,
+          httpOnly: true,
+        })
+        .status(200)
+        .send({ loginSuccess: true, userEmail: req.body.email });
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 module.exports = router;
