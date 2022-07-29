1ddbcd9fd2333c314bad31743922dfb2cfcd87c9
jaeseo <jaeserrr@ewhain.net>
2022-07-30 03:06:35 +0900

    [ADD] Login Backend inter
    - 로그인 백엔드 연동하여 계정 확인
    - user-hooks/useInput : useState를 이용하여 input
    - user-actions : 서버와의 연결하는 함수들 모아놓는 곳

---
+++ client/src/components/user-actions/LoginUser.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+/**
+ * 로그인 - 서버에 요청
+ * @param {object}
+ * dataToSubmit : 서버에 보낼 정보
+ */
+export default async function loginUser(dataToSubmit) {
+  let data;
+  try {
+    await axios.post(`/users/login`, dataToSubmit).then((response) => {
+      data = response.data;
+    });
+    return data;
+  } catch (error) {
+    switch (error.response.status) {
+      case 400:
+      case 403:
+      case 404:
+        return error.response.data;
+    }
+  }
+}
