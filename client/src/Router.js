bebc3ee3cfe0ac2b698ef239713fe09e06d0f4c5
sua bae <bsa0322@ewhain.net>
2022-08-01 10:22:01 +0900

    [ADD] often used calculet main page

--- client/src/Router.js
+++ client/src/Router.js
@@ -5,7 +5,6 @@ import Register from "./pages/Register";
 import BookmarkBar from "./components/global-component/BookmarkBar";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import OftenCalculet from "./pages/OftenCalculet";
 
 const AppRouter = () => {
   return (
@@ -17,8 +16,6 @@ const AppRouter = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
-        {/* <Route path="/" element={<Calculet />}></Route> */}
-        <Route path="/" element={<OftenCalculet />}></Route>
       </Routes>
       <BookmarkBar />
     </BrowserRouter>
--- client/src/components/atom-components/Bookmark.js
+++ client/src/components/atom-components/Bookmark.js
@@ -25,26 +25,10 @@ const StyledLink = styled(Link)`
  *
  */
 function Bookmark({ info }) {
-  function updateCalculetCount(calculetId) {
-    if (calculetId !== Number(localStorage.getItem("previewCalculet"))) {
-      console.log("update!", calculetId);
-      localStorage.setItem("previewCalculet", calculetId);
-      localStorage.setItem("continueCnt", 1);
-    } else {
-      console.log("continue");
-      const cnt = localStorage.getItem("continueCnt");
-      localStorage.setItem("continueCnt", Number(cnt) + 1);
-    }
-  }
-
   return (
     <Positioner>
       {info.map((conts, index) => (
-        <StyledLink
-          to={"/" + conts.id}
-          key={index}
-          onClick={() => updateCalculetCount(conts.id)}
-        >
+        <StyledLink to={"/" + conts.id} key={index}>
           {conts.title}
         </StyledLink>
       ))}
