723c95e10d12667a2af4654d91296bc3369bbad5
sua bae <bsa0322@ewhain.net>
2022-08-04 04:46:46 +0900

    [FIX] previous calculet name typo

--- client/src/pages/Calculet.js
+++ client/src/pages/Calculet.js
@@ -96,7 +96,7 @@ function Calculet() {
   // 현재 페이지에 로딩할 계산기 id
   let { id } = useParams();
 
-  // id 없다면 초기화 (메인 페이지)
+  // id 없다면 메인 페이지이므로 자주 쓰는 계산기 불러오기
   if (id === undefined) {
     // 자주 쓰는 계산기의 선정 기준
     const STANDARD_CNT = 3;
@@ -105,24 +105,22 @@ function Calculet() {
     const continueCnt = localStorage.getItem("continueCnt");
 
     // 이전 계산기
-    const previewCalculet = localStorage.getItem("previewCalculet");
+    const previousCalculet = localStorage.getItem("previousCalculet");
 
     // 만약 이전 계산기의 연속 횟수가 기준에 도달했다면 자주 쓰는 계산기 값 변경
     if (Number(continueCnt) === STANDARD_CNT) {
-      localStorage.setItem("oftenCalculet", previewCalculet);
+      localStorage.setItem("oftenCalculet", previousCalculet);
     }
 
     // 만약 자주 쓰는 계산기가 비어있다면 초기화
     if (localStorage.getItem("oftenCalculet") === null) {
       localStorage.setItem("oftenCalculet", 1);
-      localStorage.setItem("previewCalculet", 1);
+      localStorage.setItem("previousCalculet", 1);
       localStorage.setItem("continueCnt", 1);
     }
 
     // 자주 쓰는 계산기 가져오기
     id = localStorage.getItem("oftenCalculet");
-
-    console.log(id);
   }
 
   /**
--- client/src/utils/UpdateCalculetCount.js
+++ client/src/utils/UpdateCalculetCount.js
@@ -1,11 +1,13 @@
-export default function updateCalculetCount(calculetId) {
-  if (Number(calculetId) !== Number(localStorage.getItem("previewCalculet"))) {
-    console.log("update!", calculetId);
-    localStorage.setItem("previewCalculet", calculetId);
+function updateCalculetCount(calculetId) {
+  if (Number(calculetId) !== Number(localStorage.getItem("previousCalculet"))) {
+    // console.log("update!", calculetId);
+    localStorage.setItem("previousCalculet", calculetId);
     localStorage.setItem("continueCnt", 1);
   } else {
-    console.log("continue");
+    // console.log("continue");
     const cnt = localStorage.getItem("continueCnt");
     localStorage.setItem("continueCnt", Number(cnt) + 1);
   }
 }
+
+export default updateCalculetCount;
