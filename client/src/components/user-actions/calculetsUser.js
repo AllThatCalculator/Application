badf88dc0dbabea0a6cde96dd3f069fedf0f68d9
jaeseo <jaeserrr@ewhain.net>
2022-08-13 02:05:56 +0900

    [CHORE] comment fix

--- client/src/components/user-actions/calculetsUser.js
+++ client/src/components/user-actions/calculetsUser.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 /**
- * 계산기 정보 - 서버에 요청
+ * 계산기 정보 얻어오기 - 서버에 요청
+ * - 계산기 전체 목록 페이지, 카테고리바에서 요청
+ * - Object {대분류, 소분류에 따른 계산기 id, title}
  * -> 보낼 데이터 없음.
  */
 async function calculetsUser() {
