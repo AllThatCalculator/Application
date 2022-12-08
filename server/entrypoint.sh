a5168125963eb43249be2462db71705790918f2f
jiyoung <jiyoung_06@ewhain.net>
2022-12-09 03:14:28 +0900

    [ADD] autossh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,6 +4,6 @@
 mkdir ~/.ssh
 ssh-keyscan -H ${SSH_HOST} > ~/.ssh/known_hosts
 chmod 400 ${SSH_PEM_KEY}
-ssh -fN -4 -i "${SSH_PEM_KEY}" -L ${MYSQL_PORT}:${RDS_HOST}:${MYSQL_PORT} ${SSH_USER}@${SSH_HOST}
+autossh -fN -4 -i "${SSH_PEM_KEY}" -L ${MYSQL_PORT}:${RDS_HOST}:${MYSQL_PORT} ${SSH_USER}@${SSH_HOST}
 
 npx nodemon -L main.js
