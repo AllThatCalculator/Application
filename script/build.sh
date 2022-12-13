8338839f1546a6d4c222e5de45e7bb4d87b67ed7
jiyoung <jiyoung_06@ewhain.net>
2022-12-13 21:55:25 +0900

    [DELETE] database from production mode

--- script/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker-compose down
-
-rm -rf ../client/node_modules
-rm -rf ../server/node_modules
-
-rm -rf ../database/mariaDB/data
-
-docker-compose -f ./docker-compose.yml up --build
