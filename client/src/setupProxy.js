479d26c9b1c88269986906279a1771a2d2386d39
jiyoung <jiyoung_06@ewhain.net>
2022-12-28 14:57:13 +0900

    [FIX] proxy setting for local frontend

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,19 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    ["/api", "/file"],
     createProxyMiddleware({
-      target: "http://backend:5000",
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api": "", // URL ^/api -> 공백 변경
-      },
-    })
-  );
-  app.use(
-    "/file",
-    createProxyMiddleware({
-      target: "http://nginx:80",
+      target: process.env.REACT_APP_BACKEND_URL,
       changeOrigin: true,
     })
   );
