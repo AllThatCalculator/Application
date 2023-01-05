392f1476e6ca80e4ff269b1c1cb238cfa9dac0d6
suabae <bsa0322@ewhain.net>
2023-01-06 03:29:19 +0900

    [FIX] sql to orm in user signup

--- server/routes/users/signUp.js
+++ server/routes/users/signUp.js
@@ -1,35 +1,27 @@
-const mariadb = require("../../config/database");
+const { models } = require("../../models");
 
 exports.signUp = async (req, res) => {
-  // 프로필 이미지 blob으로 변경
-  // 프론트엔드에서 전달받은 base64String
-  let base64String = req.body.profileImg;
-  // blob으로 변경해서 저장할 변수
-  let blobImage = null;
-  // 기본 이미지가 아닌 경우에만 blob으로 decode
-  if (base64String !== "/img/defaultProfile.png") {
-    blobImage = base64String.split(";base64,").pop();
-    blobImage = Buffer.from(blobImage, "base64");
-  }
-
-  // 데이터 삽입
-  const userInfoData = [
-    req.body.id,
-    req.body.email,
-    req.body.userName,
-    blobImage,
-    req.body.bio,
-    req.body.sex,
-    req.body.birthdate,
-    req.body.job,
-  ];
-
-  const userInfoQuery = `INSERT INTO user_info VALUES(?,?,?,?,?,?,?,?);`;
-
-  const sql1 = mariadb.format(userInfoQuery, userInfoData);
+  // // 프로필 이미지 blob으로 변경
+  // // 프론트엔드에서 전달받은 base64String
+  // let base64String = req.body.profileImg;
+  // // blob으로 변경해서 저장할 변수
+  // let blobImage = null;
+  // // 기본 이미지가 아닌 경우에만 blob으로 decode
+  // if (base64String !== "/img/defaultProfile.png") {
+  //   blobImage = base64String.split(";base64,").pop();
+  //   blobImage = Buffer.from(blobImage, "base64");
+  // }
 
   try {
-    await mariadb.query(sql1);
+    await models.userInfo.create({
+      id: req.body.id,
+      email: req.body.email,
+      user_name: req.body.userName,
+      bio: req.body.bio,
+      sex: req.body.sex,
+      birthdate: req.body.birthdate,
+      job: req.body.job,
+    });
     console.log("success sign up");
     res.status(201).send({
       success: true,
