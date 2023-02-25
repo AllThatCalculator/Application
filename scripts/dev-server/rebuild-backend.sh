c0ec66b39ab760f6dbd209a827b2f4c9150c184b
jiyoung <jiyoung_06@ewhain.net>
2023-02-26 03:11:31 +0900

    [ADD] backend deploy script

--- scripts/dev-server/rebuild-backend.sh
+++ scripts/dev-server/rebuild-backend.sh
@@ -2,24 +2,24 @@
 
 git pull
 
-RUNNING_APPLICATION=$(docker ps | grep api_blue)
-DEFAULT_CONF="./nginx/nginx.dev-server/nginx.conf"
+IS_BLUE_RUNNING=$(docker ps | grep api_blue)
+NGINX_CONFIG_FILE="./nginx/nginx.dev-server/nginx.conf"
 
 ##### BLUE RUNNING...
-if [ -n "$RUNNING_APPLICATION"  ];then
-  CONTAINER_A="api_green"
-  CONTAINER_B="api_blue"
+if [ -n "$IS_BLUE_RUNNING"  ];then
+  CONTAINER_A="api_green" # to deploy
+  CONTAINER_B="api_blue"  # to stop
 else
   CONTAINER_A="api_blue"
   CONTAINER_B="api_green"
 fi
 
-# Start container A and stop container B
+# Start container A
 echo "Deploy $CONTAINER_A..."
-
 docker compose -f ./docker-compose.dev-server.yml build $CONTAINER_A
 docker compose -f ./docker-compose.dev-server.yml up -d $CONTAINER_A
 
+# wait until backend server starts
 while [ 1 == 1 ]; do
   echo "$CONTAINER_A health check...."
   REQUEST=$(docker exec frontend curl http://$CONTAINER_A:5000)
@@ -30,12 +30,17 @@ while [ 1 == 1 ]; do
   sleep 3
 done;
 
-sed -i "s/$CONTAINER_B/$CONTAINER_A/g" $DEFAULT_CONF
+# toggle nginx config file
+sed -i "s/$CONTAINER_B/$CONTAINER_A/g" $NGINX_CONFIG_FILE
 docker exec frontend nginx -s reload
 
+# preserver logs
 log_path="./log/$(TZ="Asia/Seoul" date '+%Y-%m-%d-%H-%M')-$CONTAINER_B.out"
 docker logs $CONTAINER_B > $log_path
 
+# stop container B
 docker compose -f ./docker-compose.dev-server.yml stop $CONTAINER_B
+
+# clear unusing docker
 docker image prune -af
 docker system prune -f
