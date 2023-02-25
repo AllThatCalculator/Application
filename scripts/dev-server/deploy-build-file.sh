d8f093a4acb924d36b7e2c3955e58bd79b6a6ecc
jiyoung <jiyoung_06@ewhain.net>
2023-02-26 00:51:47 +0900

    [ADD] zero downtime deploy script

--- scripts/dev-server/deploy-build-file.sh
+++ scripts/dev-server/deploy-build-file.sh
@@ -3,3 +3,4 @@
 git pull
 cd ./client
 sudo tar -xzvf build.tar.gz
+docker exec frontend nginx -s reload
--- scripts/dev-server/rebuild-backend.sh
+++ scripts/dev-server/rebuild-backend.sh
@@ -2,6 +2,33 @@
 
 git pull
 
-docker exec api npm install
+RUNNING_APPLICATION=$(docker ps | grep api_blue)
+DEFAULT_CONF="./nginx/nginx.dev-server/nginx.conf"
 
-docker compose -f ./docker-compose.dev-server.yml restart api
+##### BLUE RUNNING...
+if [ -n "$RUNNING_APPLICATION"  ];then
+  CONTAINER_A="api_green"
+  CONTAINER_B="api_blue"
+else
+  CONTAINER_A="api_blue"
+  CONTAINER_B="api_green"
+fi
+
+# Start container A and stop container B
+echo "Deploy $CONTAINER_A..."
+docker compose -f ./docker-compose.dev-server.yml build $CONTAINER_A
+docker compose -f ./docker-compose.dev-server.yml up -d $CONTAINER_A
+
+while [ 1 == 1 ]; do
+  echo "$CONTAINER_A health check...."
+  REQUEST=$(docker exec nginx curl http://$CONTAINER_A:5000)
+  echo $REQUEST
+  if [ -n "$REQUEST" ]; then
+    break ;
+  fi
+  sleep 3
+done;
+
+sed -i "s/$CONTAINER_B/$CONTAINER_A/g" $DEFAULT_CONF
+docker exec frontend nginx -s reload
+docker compose -f ./docker-compose.dev-server.yml stop $CONTAINER_B
