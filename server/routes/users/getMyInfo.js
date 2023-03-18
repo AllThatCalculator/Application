a54c5d2bad8dbc65c457f9e7d2b8f95126064e55
jiyoung <jiyoung_06@ewhain.net>
2023-03-18 21:02:10 +0900

    [FIX] calculet api

--- server/routes/users/getMyInfo.js
+++ server/routes/users/getMyInfo.js
@@ -1,5 +1,3 @@
-const { urlFormatter } = require("../../utils/urlFormatter");
-
 /**
  * 유저 프로필(헤더 디스플레이 - userName, profileImg) 찾는 함수
  */
@@ -9,7 +7,7 @@ function getUserSimpleInfo(req, res) {
   // 데이터 가공
   const userInfo = {
     userName: user.user_name,
-    profileImgSrc: urlFormatter("profileImg", user.profile_img),
+    profileImgSrc: user.profile_img,
   };
 
   res.status(200).send(userInfo);
@@ -29,7 +27,7 @@ function getUserInfo(req, res) {
     sex: user.sex,
     birthdate: user.birthdate,
     job: user.job,
-    profileImgSrc: urlFormatter("profileImg", user.profile_img),
+    profileImgSrc: user.profile_img,
     email: user.email,
   };
 
