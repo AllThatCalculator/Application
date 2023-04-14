1ed5d6f2cae652fac632d020c0879762aeead9d5
jiyoung <jiyoung_06@ewhain.net>
2023-04-15 02:15:20 +0900

    [CHORE] refactoring

--- server/admin/resources/userInfo.js
+++ server/admin/resources/userInfo.js
@@ -25,6 +25,55 @@ async function verifyAccessLevel(userId) {
 }
 
 /**
+ * firebase field 등록 및 admin table에 내용 반영
+ * @param {object} userRecord
+ * @param {number} grantLevel 부여할 레벨
+ * @param {object} currentAdmin 관리자
+ * @returns 유저의 직전 레벨
+ */
+async function grantAdminAccess(userRecord, grantLevel, currentAdmin) {
+  const prevLevel = await verifyAccessLevel(userRecord.id);
+
+  // 본인보다 높은 권한의 유저
+  if (prevLevel >= currentAdmin.accessLevel) {
+    throw Error("권한이 없습니다.");
+  }
+
+  if (prevLevel === grantLevel) {
+    throw Error("기존 권한과 동일함");
+  }
+
+  const newCustomClaims = {
+    registered: true,
+    admin: grantLevel !== 0,
+    accessLevel: grantLevel,
+  };
+
+  // update firebase custom claim
+  await admin.auth().setCustomUserClaims(userRecord.id, newCustomClaims);
+
+  const adminUser = await models.admin.findByPk(userRecord.id);
+
+  if (!!adminUser) {
+    if (grantLevel === 0) {
+      // to normal user
+      await adminUser.destroy();
+    } else {
+      // update accessLevel
+      await adminUser.update({ accessLevel: grantLevel });
+    }
+  } else {
+    await models.admin.create({
+      id: userRecord.id,
+      email: userRecord.email,
+      accessLevel: grantLevel,
+    });
+  }
+
+  return prevLevel;
+}
+
+/**
  * grantLevel 부여하는 액션, 최소 managerLevel 이상 가능
  * @param {number} grantLevel 부여할 레벨
  * @param {number} managerLevel minimum level required
@@ -43,48 +92,17 @@ const grantAdminAccessAction = (grantLevel, managerLevel) => ({
       if (res.locals.accessLevel !== currentAdmin.accessLevel) {
         throw Error("권한 확인 불가");
       }
-      const currentLevel = await verifyAccessLevel(record.params.id);
 
-      // 본인보다 높은 권한의 유저
-      if (currentLevel >= currentAdmin.accessLevel) {
-        throw Error("권한이 없습니다.");
-      }
-
-      if (currentLevel === grantLevel) {
-        throw Error("기존 권한과 동일함");
-      }
-
-      const newCustomClaims = {
-        registered: true,
-        admin: grantLevel !== 0,
-        accessLevel: grantLevel,
-      };
-
-      // update firebase custom claim
-      await admin.auth().setCustomUserClaims(record.params.id, newCustomClaims);
-
-      const adminUser = await models.admin.findByPk(record.params.id);
-
-      if (!!adminUser) {
-        if (grantLevel === 0) {
-          // to normal user
-          await adminUser.destroy();
-        } else {
-          // update accessLevel
-          await adminUser.update({ accessLevel: grantLevel });
-        }
-      } else {
-        await models.admin.create({
-          id: record.params.id,
-          email: record.params.email,
-          accessLevel: grantLevel,
-        });
-      }
+      const prevLevel = await grantAdminAccess(
+        record.params,
+        grantLevel,
+        currentAdmin
+      );
 
       return {
         record: record.toJSON(currentAdmin),
         notice: {
-          message: `Access level: ${currentLevel} -> ${grantLevel}`,
+          message: `Access level: ${prevLevel} -> ${grantLevel}`,
           type: "success",
         },
       };
