f23a4dd5e34d9173bfc4e6fb1cf5886d978d02a3
jiyoung <jiyoung_06@ewhain.net>
2023-04-15 02:03:16 +0900

    [ADD] link admin claim & admin table

--- server/admin/resources/userInfo.js
+++ server/admin/resources/userInfo.js
@@ -1,5 +1,5 @@
 const { admin } = require("../../config/firebase");
-const { models } = require("../../models");
+const { models, sequelize } = require("../../models");
 const { accessController } = require("../utils/accessController");
 
 /**
@@ -7,17 +7,21 @@ const { accessController } = require("../utils/accessController");
  * @param {string} userId firebase uid
  * @returns accessLevel of user
  */
-function verifyAccessLevel(userId) {
-  return admin.auth().getUser(userId).then((user) => {
-    console.log(user);
-    if (user.customClaims.admin) {
-      return user.customClaims.accessLevel;
-    }
-    return 0;
-  }).catch((error) => {
-    console.error(error);
-    return 0;
-  });
+async function verifyAccessLevel(userId) {
+  return admin
+    .auth()
+    .getUser(userId)
+    .then((user) => {
+      console.log(user);
+      if (user.customClaims.admin) {
+        return user.customClaims.accessLevel;
+      }
+      return 0;
+    })
+    .catch((error) => {
+      console.error(error);
+      return 0;
+    });
 }
 
 /**
@@ -26,7 +30,7 @@ function verifyAccessLevel(userId) {
  * @param {number} managerLevel minimum level required
  */
 const grantAdminAccessAction = (grantLevel, managerLevel) => ({
-  isAccessible: ({ currentAdmin }) => (currentAdmin.accessLevel >= managerLevel),
+  isAccessible: ({ currentAdmin }) => currentAdmin.accessLevel >= managerLevel,
   actionType: "record",
   component: false,
   handler: async (req, res, context) => {
@@ -41,41 +45,63 @@ const grantAdminAccessAction = (grantLevel, managerLevel) => ({
       }
       const currentLevel = await verifyAccessLevel(record.params.id);
 
+      // 본인보다 높은 권한의 유저
       if (currentLevel >= currentAdmin.accessLevel) {
         throw Error("권한이 없습니다.");
       }
 
-      const newCustomClaims = grantLevel === 0 ? {
+      if (currentLevel === grantLevel) {
+        throw Error("기존 권한과 동일함");
+      }
+
+      const newCustomClaims = {
         registered: true,
-        admin: false
-      } : {
-        registered: true,
-        admin: true,
-        accessLevel: grantLevel
+        admin: grantLevel !== 0,
+        accessLevel: grantLevel,
       };
 
+      // update firebase custom claim
       await admin.auth().setCustomUserClaims(record.params.id, newCustomClaims);
+
+      const adminUser = await models.admin.findByPk(record.params.id);
+
+      if (!!adminUser) {
+        if (grantLevel === 0) {
+          // to normal user
+          await adminUser.destroy();
+        } else {
+          // update accessLevel
+          await adminUser.update({ accessLevel: grantLevel });
+        }
+      } else {
+        await models.admin.create({
+          id: record.params.id,
+          email: record.params.email,
+          accessLevel: grantLevel,
+        });
+      }
 
       return {
         record: record.toJSON(currentAdmin),
         notice: {
           message: `Access level: ${currentLevel} -> ${grantLevel}`,
-          type: "success"
-        }
+          type: "success",
+        },
       };
     } catch (error) {
+      console.error(error);
       return {
         record: record.toJSON(currentAdmin),
         notice: {
           message: `관리자 등록 실패 - ${error.message}`,
-          type: "error"
-        }
+          type: "error",
+        },
       };
     }
   },
   guard: "등록하시겠습니까?",
   icon: `Number_${grantLevel}`,
-  parent: "grantAdmin"
+  parent: "grantAdmin",
 });
 
 const userInfoResource = {
@@ -87,10 +113,10 @@ const userInfoResource = {
 userInfoResource.options.actions = {
   grantAdmin: {
     icon: "UserAdmin",
-    actionType: "record"
+    actionType: "record",
   },
   showAccessLevel: {
-    isAccessible: ({ currentAdmin }) => (currentAdmin.accessLevel >= 4),
+    isAccessible: ({ currentAdmin }) => currentAdmin.accessLevel >= 4,
     actionType: "record",
     component: false,
     handler: async (req, res, context) => {
@@ -101,8 +127,8 @@ userInfoResource.options.actions = {
           record: record.toJSON(currentAdmin),
           notice: {
             message: `accessLevel - ${accessLevel}`,
-            type: "success"
-          }
+            type: "success",
+          },
         };
       } catch (error) {
         console.log(error);
@@ -110,22 +136,22 @@ userInfoResource.options.actions = {
           record: record.toJSON(currentAdmin),
           notice: {
             message: `조회 실패 - ${error.message}`,
-            type: "error"
-          }
+            type: "error",
+          },
         };
       }
     },
     icon: "Checkmark",
-    parent: "grantAdmin"
+    parent: "grantAdmin",
   },
   asNormalUser: {
     ...grantAdminAccessAction(0, 4),
-    icon: "Unlink"
+    icon: "Unlink",
   },
   asViewer: grantAdminAccessAction(1, 4),
   asPublisher: grantAdminAccessAction(2, 4),
   asManager: grantAdminAccessAction(3, 4),
-  asExecutive: grantAdminAccessAction(4, 9)
+  asExecutive: grantAdminAccessAction(4, 9),
 };
 
 module.exports = { userInfoResource };
