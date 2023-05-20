4d6014a94e68907834f9681d7c01b791a0eda000
suabae <bsa0322@ewhain.net>
2023-05-21 07:03:32 +0900

    [REFACTOR] update api

--- server/routes/users/updateMyCalculet.js
+++ server/routes/users/updateMyCalculet.js
@@ -2,51 +2,74 @@ const { models } = require("../../models");
 const { CustomError } = require("../../utils/CustomError");
 const { matchedData } = require("express-validator");
 
+/**
+ * 계산기 ID에 대한 key가 주어졌을 때, where 옵션을 리턴하는 함수
+ * @param {*} calculetKey 계산기 ID 필드 이름 (id or calculetId)
+ * @param {*} calculetInfo 계산기 정보
+ * @returns object
+ */
+function getWhereOption(calculetKey, calculetInfo) {
+  return {
+    where: {
+      [calculetKey]: calculetInfo.id,
+      contributorId: calculetInfo.contributorId,
+    },
+  };
+}
+
+/**
+ * calculet_info, calculet_info_temp 테이블을 조회해서 각각 계산기를 얻어오는 함수
+ * @param {*} calculetInfo 계산기 정보들
+ */
+async function getCalculet(calculetInfo, whereIdOption) {
+  if (calculetInfo.blocked === 2) {
+    return await models.calculetInfoTemp.findOne(whereIdOption);
+  }
+  // check exist calculet temp (because if exist calculet temp, then should modify temp table, not info)
+  const temp = await models.calculetInfoTemp.findOne(
+    getWhereOption("calculetId", calculetInfo)
+  );
+  if (temp) {
+    console.log("Editing temp calculet is exist");
+    throw new CustomError(400, 1);
+  }
+  return await models.calculetInfo.findOne(whereIdOption);
+}
+
+/**
+ * [고려해야 할 케이스]
+ *
+ *                                   <calculetTemp 삭제> <update log 삭제> <registered> <calculetTemp 생성> <update log 생성>
+ * 1. 첫 등록 후 등록 대기 중인 계산기         O                  X             False             O                  X
+ * 2. 등록 이후 수정 요청 중인 계산기          O                  O             True              O                  O
+ * 3. 등록 완료된 계산기 (수정 중 없음)        X                  X             True              O                  O
+ */
 async function updateMyCalculet(req, res) {
   const bodyData = matchedData(req, { locations: ["body"] });
   const { calculetInfo, updateMessage } = bodyData;
 
-  const whereOption = {
-    where: {
-      id: calculetInfo.id,
-      contributorId: res.locals.userId,
-    },
-  };
+  // set calculet id & contributor id
+  calculetInfo.calculetId = calculetInfo.id;
+  calculetInfo.contributorId = res.locals.userId;
 
-  let calculet = null;
-  let calculetTemp = null;
-  let calculetId = calculetInfo.id;
-  if (calculetInfo.blocked === 2) {
-    calculetTemp = await models.calculetInfoTemp.findOne(whereOption);
-  } else {
-    // check exist calculet temp (because if exist calculet temp, then should modify temp table, not info)
-    const temp = await models.calculetInfoTemp.findOne({
-      where: {
-        calculetId: calculetId,
-        contributorId: res.locals.userId,
-      },
-    });
-    if (temp) {
-      throw new CustomError(400, 1);
-    }
-    calculet = await models.calculetInfo.findOne(whereOption);
-  }
+  const whereIdOption = getWhereOption("id", calculetInfo);
+  const calculet = await getCalculet(calculetInfo, whereIdOption);
 
-  if (calculet === null && calculetTemp === null) {
+  if (calculet === null) {
     throw new CustomError(400, 0);
   }
 
-  // check exist calculet temp data
-  if (calculetTemp !== null) {
-    calculetId = calculetTemp.calculetId;
+  // if blocked === 2, then delete temp & update log
+  if (calculetInfo.blocked === 2) {
+    calculetInfo.calculetId = calculet.calculetId;
 
     // delete temp data
-    await models.calculetInfoTemp.destroy(whereOption);
+    await models.calculetInfoTemp.destroy(whereIdOption);
 
     // if registered is true, then delete update log (latest)
-    if (calculetTemp.registered) {
+    if (calculet.registered) {
       const latestUpdateLog = await models.calculetUpdateLog.findOne({
-        where: { calculetId: calculetId },
+        where: { calculetId: calculet.calculetId },
         order: [["createdAt", "DESC"]],
         limit: 1,
       });
@@ -54,10 +77,14 @@ async function updateMyCalculet(req, res) {
     }
   }
 
-  if (calculet !== null || (calculetTemp !== null && calculetTemp.registered)) {
+  // if only calculet info or calculet temp & registered true
+  if (
+    calculetInfo.blocked !== 2 ||
+    (calculetInfo.blocked === 2 && calculet.registered)
+  ) {
     // create update log
     await models.calculetUpdateLog.create({
-      calculetId: calculetId,
+      calculetId: calculetInfo.calculetId,
       message: updateMessage,
     });
 
@@ -67,8 +94,6 @@ async function updateMyCalculet(req, res) {
 
   // create calculet info temp
   delete calculetInfo.id;
-  calculetInfo.calculetId = calculetId;
-  calculetInfo.contributorId = res.locals.userId;
   await models.calculetInfoTemp.create(calculetInfo);
   res.status(204).send();
 }
