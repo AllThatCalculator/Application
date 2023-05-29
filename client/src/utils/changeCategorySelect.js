12ae43126a254eed71e5034dffc3dd7db361b421
suabae <bsa0322@ewhain.net>
2023-05-29 19:20:46 +0900

    [FIX] post calculet (sub id 0)

--- client/src/utils/changeCategorySelect.js
+++ client/src/utils/changeCategorySelect.js
@@ -1,3 +1,5 @@
+import { ID_MAIN_ETC } from "../constants/calculetList";
+
 /**
  * 계산기 대분류 change 함수
  * - value에 먼저 접근한 후, value에 맞는 name을 찾아서 저장
@@ -6,7 +8,9 @@
 function changeCategoryMain(value, setCategoryMainId, setCategorySubId) {
   const targetValue = value !== "" ? Number(value) : "";
   setCategoryMainId(targetValue);
-  setCategorySubId(""); // 초기화
+  // 대분류 기타인지 확인
+  const subValue = Number(value) === Number(ID_MAIN_ETC) ? Number(value) : "";
+  setCategorySubId(subValue); // 초기화
 }
 
 /**
