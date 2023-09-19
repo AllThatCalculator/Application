411f49036c319915ad54ec072aebb7b6c6bf5c7f
suabae <bsa0322@ewhain.net>
2023-09-19 11:58:18 +0900

    [ADD] component property input validate

--- client/src/components/organisms/register-editor/ComponentForm.js
+++ client/src/components/organisms/register-editor/ComponentForm.js
@@ -4,45 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "@mui/material";
 import { Common, Components, Option } from "./ComponentOptions";
 import { onUpdateComponent } from "../../../modules/calculetEditor";
-import {
-  // PROPERTY_TYPE_STRING,
-  // PROPERTY_TYPE_BOOLEAN,
-  // PROPERTY_TYPE_SELECT,
-  PROPERTY_OPTION_START_NUMBER,
-  // PROPERTY_TYPE_DATE,
-} from "../../../constants/calculetComponent";
+import { PROPERTY_OPTION_START_NUMBER } from "../../../constants/calculetComponent";
 
-// import DatePickerComponent from "./DatePickerComponent";
-// import SelectComponent from "./SelectComponent";
-// import CheckboxComponent from "./CheckboxComponent";
 import Transformer from "./Transformer";
-
-// /**
-//  * 컴포넌트 속성 하나에 대한 정보를 받아서, 인풋 필드로 바꿔주는 함수
-//  * @param {string} id 컴포넌트 속성들의 id 값
-//  * @param {object} data 컴포넌트 속성에 대한 정보 (속성을 입력받는 인풋 타입이나 라벨 정보, 필수로 입력받아야 하는지 등등)
-//  * @param {string} value 컴포넌트 속성의 value 값
-//  * @param {function} onChange 컴포넌트 속성의 onChange 함수
-//  * @returns
-//  */
-// function TransformField(props) {
-//   const { type, defaultValue, ...properties } = props;
-//   if (properties.value === undefined) {
-//     properties.value = defaultValue;
-//   }
-//   switch (type) {
-//     case PROPERTY_TYPE_STRING:
-//       return <TextField {...properties} />;
-//     case PROPERTY_TYPE_BOOLEAN:
-//       return <CheckboxComponent {...properties} />;
-//     case PROPERTY_TYPE_SELECT:
-//       return <SelectComponent {...properties} />;
-//     case PROPERTY_TYPE_DATE:
-//       return <DatePickerComponent {...properties} />;
-//     default:
-//       return;
-//   }
-// }
+import {
+  validateAllComponents,
+  validateOneComponent,
+} from "./validateComponentProperties";
 
 /**
  * 컴포넌트 type에 따라 속성을 입력하는 필드들을 모은 컴포넌트
@@ -52,6 +20,7 @@ import Transformer from "./Transformer";
  */
 function ComponentForm({ componentId, componentType }) {
   const dispatch = useDispatch();
+  const components = useSelector((state) => state.calculetEditor.components);
   const inputs = useSelector(
     (state) => state.calculetEditor.components[componentId]
   ); // 컴포넌트 속성에 대한 인풋값
@@ -70,36 +39,16 @@ function ComponentForm({ componentId, componentType }) {
     }
   }, [inputs.options, properties.options]);
 
-  // // 컴포넌트 속성 인풋 onChange 함수
-  // const onInputsChange = useCallback(
-  //   (e) => {
-  //     let { id, value } = e.target; // id: componentOption의 key값 (속성 이름)
-  //     if (id && properties[id].type === PROPERTY_TYPE_BOOLEAN) {
-  //       value = e.target.checked;
-  //     } else if (id === undefined) {
-  //       id = e.target.name;
-  //     }
-  //     dispatch(onUpdateComponent({ componentId, targetId: id, value }));
-  //   },
-  //   [properties, componentId, dispatch]
-  // );
-
-  // TODO 입력 검증 필요
-  // // 속성이 유효한지 확인하는 함수
-  // const invalidComponentOption = useCallback(
-  //   (data) => {
-  //     for (const key in properties) {
-  //       if (properties[key].required && !inputs[key]) {
-  //         return false;
-  //       }
-  //     }
-  //     if (!data.isInput && !data.isOutput) {
-  //       return false;
-  //     }
-  //     return true;
-  //   },
-  //   [properties, inputs]
-  // );
+  // 속성이 유효한지 확인하는 함수
+  const invalidComponentOption = useCallback(() => {
+    for (const key in components) {
+      console.log(
+        components[key].id,
+        validateOneComponent(components, components[key])
+      );
+    }
+    console.log("all", validateAllComponents(components));
+  }, [components]);
 
   // 옵션 추가하는 함수
   const addOption = useCallback(() => {
@@ -148,6 +97,7 @@ function ComponentForm({ componentId, componentType }) {
 
   // console.log("속성 컴포넌트 정보", properties);
   // console.log("컴포넌트 옵션 정보", inputs);
+  invalidComponentOption();
 
   return (
     <Grid container sx={{ backgroundColor: "white" }}>
---
+++ client/src/components/organisms/register-editor/validateComponentProperties.js
@@ -0,0 +1,102 @@
+import { Components } from "./ComponentOptions";
+
+const TEXT_LIMIT = { label: 10 };
+
+/**
+ * 컴포넌트 하나에 필요한 속성 모두 입력되었는지 검사
+ * @param {object} inputs: 컴포넌트 속성 입력값
+ * @param {object} properties: 컴포넌트 속성을 컴포넌트로 만들기 위한 속성 정보
+ */
+function validateOneComponent(components, inputs) {
+  const properties = Components[inputs.componentType];
+  for (const key in properties) {
+    if (
+      Boolean(properties[key].required) &&
+      (!inputs[key] || Object.keys(inputs[key]).length === 0)
+    ) {
+      // 필수로 입력되어야 하는 속성인데, inputs에 없다면
+      return false;
+    }
+    if (
+      // 글자 수 제한 있는 컴포넌트가 길이를 넘었다면
+      Object.keys(TEXT_LIMIT).includes(key) &&
+      !validateCharacterLimit(String(inputs[key]), TEXT_LIMIT[key])
+    ) {
+      return false;
+    }
+  }
+  if (Boolean(validateDuplicatedId(components, inputs)) === false) {
+    return false;
+  }
+  if (inputs.options) {
+    for (const key in inputs.options) {
+      if (
+        !validateDuplicateOptionValue(inputs.options, key, inputs.options[key])
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+/**
+ * 변수명 중복 검사
+ * @param {object} components: 컴포넌트 속성 입력값 객체를 여러 개 담고있는 컴포넌트 객체
+ * @param {object} target: 확인하려는 컴포넌트의 속성
+ */
+function validateDuplicatedId(components, target) {
+  if (!target.id) {
+    return null;
+  }
+  for (const key in components) {
+    if (String(target.componentId) === String(components[key].componentId)) {
+      continue;
+    }
+    if (String(target.id) === String(components[key].id)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 옵션 아이템 value값 중복 검사
+function validateDuplicateOptionValue(options, targetKey, target) {
+  if (!target.value) {
+    return null;
+  }
+  for (const optionKey in options) {
+    if (String(optionKey) === String(targetKey)) {
+      continue;
+    }
+    if (String(options[optionKey].value) === String(target.value)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 글자 수 제한하는 속성(ex. label)에 대한 검사
+function validateCharacterLimit(targetText, limit) {
+  if (targetText.length > limit) {
+    return false;
+  }
+  return true;
+}
+
+// 모든 컴포넌트 입력 완료되었는지 검사
+function validateAllComponents(components) {
+  for (const key in components) {
+    if (!validateOneComponent(components, components[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export {
+  validateOneComponent,
+  validateAllComponents,
+  validateCharacterLimit,
+  validateDuplicatedId,
+};
